4111ae2adc62f98fe3f5cd11adfd2510f4d19398
DDD <77850088+D-R-D@users.noreply.github.com>
2023-07-17 13:21:14 +0900

    動作確認の取れている機能に変更、ディレクトリの初期化クラス追加

---
+++ discord_template/DirectoryInit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discord_template
+{
+    internal class DirectoryInit
+    {
+        public static void init()
+        {
+            checkdir("commands");
+        }
+
+        private static void checkdir(string dirname)
+        {
+            if (!Directory.Exists($"{Directory.GetCurrentDirectory()}/{dirname}"))
+            {
+                Directory.CreateDirectory($"{Directory.GetCurrentDirectory()}/{dirname}");
+            }
+        }
+
+        private static void checkfile(string filename)
+        {
+            if (!File.Exists($"{Directory.GetCurrentDirectory()}/{filename}"))
+            {
+                File.Create($"{Directory.GetCurrentDirectory()}/{filename}").Close();
+            }
+        }
+    }
+}
